bb04295b7f6e2165144b12d977a770fb2e4ca48c
davismohar <davismohar@gmail.com>
2018-08-29 14:13:52 -0400

    Post time is comverted to datetime object

--- Scraper.py
+++ Scraper.py
@@ -14,19 +14,19 @@ print date
 for link in soup.body.section.find_all("li"):
     #finds the post time
     timeIndex = str(link.p).find("datetime") + 10
-    year = str(link.p)[timeIndex: timeIndex + 4]
-    month = str(link.p)[timeIndex + 5: timeIndex + 7]
-    day = str(link.p)[timeIndex + 8: timeIndex + 10]
-    hour = str(link.p)[timeIndex + 11: timeIndex + 13]
-    minute = str(link.p)[timeIndex + 14: timeIndex + 16]
-    #finds the title of the post
-    titleStartIndex = str(link.p).find(".html") + 7
-    titleEndIndex = str(link.p).find("</a>", titleStartIndex, len(str(link.p)))
-    title = str(link.p)[titleStartIndex: titleEndIndex]
-
-    datetime.datetime(year, month, day)
-    print(year + " " + month + " " + day + " " + hour + " " + minute)
-
-    print(title)
+    if timeIndex > 30:
+        year = int(str(link.p)[timeIndex: timeIndex + 4])
+        month = int(str(link.p)[timeIndex + 5: timeIndex + 7])
+        day = int(str(link.p)[timeIndex + 8: timeIndex + 10])
+        hour = int(str(link.p)[timeIndex + 11: timeIndex + 13])
+        minute = int(str(link.p)[timeIndex + 14: timeIndex + 16])
+        #finds the title of the post
+        titleStartIndex = str(link.p).find(".html") + 7
+        titleEndIndex = str(link.p).find("</a>", titleStartIndex, len(str(link.p)))
+        title = str(link.p)[titleStartIndex: titleEndIndex]
+        time = datetime.datetime(year, month, day, hour, minute)
+        print(time)
+        print(title)
+        print("")
 
     

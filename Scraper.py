aab47b9bb50c13f3afebe07696222c0cdf02d6fc
Davis Mohar <davismohar@users.noreply.github.com>
2018-10-25 12:26:08 -0400

    Added functions

--- Scraper.py
+++ Scraper.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import datetime
 import smtplib
 
+#email server
+server = "stmp.gmail.com"
 #put url here
 URL = ""
 #how often this program is run (in minutes)
@@ -23,71 +25,72 @@ Subject: %s
 
 
 """%username, recipient, subject
-
-#connects to the email server
-try:
-    server = smtplib.SMTP("smtp.gmail.com", 587)
-    server.starttls()
-    server.ehlo()
-    print("Connect success")
-    server.login(username, password)
-except:
-    print("Failed to connect to server")
-    msg = "failed to authenticate, please make sure email settings are correct"
-    server.sendmail(username, recipient, msg) 
-
-
+def connect(server, username, password):
+    #connects to the email server
+    try:
+        server = smtplib.SMTP(server, 587)
+        server.starttls()
+        server.ehlo()
+        print("Connect success")
+        server.login(username, password)
+    except:
+        print("Failed to connect to server")
+        msg = "failed to authenticate, please make sure email settings are correct"
+        server.sendmail(username, recipient, msg) 
 
 
+def parse():
+    soup = BeautifulSoup(page, "html.parser")
+    #splits the homepage into the posts
+    for link in soup.body.section.find_all("li"):
+        #finds the post time
+        timeIndex = str(link.p).find("datetime") + 10
+        #This was a simple way to prevent other links from showing up
+        if timeIndex > 30:
+            year = int(str(link.p)[timeIndex: timeIndex + 4])
+            month = int(str(link.p)[timeIndex + 5: timeIndex + 7])
+            day = int(str(link.p)[timeIndex + 8: timeIndex + 10])
+            hour = int(str(link.p)[timeIndex + 11: timeIndex + 13])
+            minute = int(str(link.p)[timeIndex + 14: timeIndex + 16])
+            time = datetime.datetime(year, month, day, hour, minute)
+            timedifference = datetime.datetime.now() - time
+            #if the post timestamp is within the update rate, then it is considered a new post and displayed
+            if(timedifference.days == 0 and timedifference.seconds < UPDATE_RATE*60):
+                #finds the title of the post
+                titleStartIndex = str(link.p).find(".html") + 7
+                titleEndIndex = str(link.p).find("</a>", titleStartIndex, len(str(link.p)))
+                title = str(link.p)[titleStartIndex: titleEndIndex]
+                #finds the price
+                priceStartIndex = int(str(link.p).find("result-price") + 15)
+                priceEndIndex = str(link.p).find("</span>", priceStartIndex, len(str(link.p)))
+                #if there is no price, then default to 0
+                if (priceStartIndex > 100):
+                    price = str(link.p)[priceStartIndex: priceEndIndex]
+                else:
+                    price = 0
+                #finds the link
+                addressStartIndex = str(link.p).find("href") + 6
+                addressEndIndex = titleStartIndex - 2
+                address = str(link.p)[addressStartIndex: addressEndIndex]
+                
+                msg = msg + title + "\n" + "time: " + str(time) + "\nprice: " + price + "\n" + address + "\n\n\n\n"
 
+                
 
-soup = BeautifulSoup(page, "html.parser")
-date = datetime.datetime.now()
-#splits the homepage into the posts
-for link in soup.body.section.find_all("li"):
-    #finds the post time
-    timeIndex = str(link.p).find("datetime") + 10
-    #This was a simple way to prevent other links from showing up
-    if timeIndex > 30:
-        year = int(str(link.p)[timeIndex: timeIndex + 4])
-        month = int(str(link.p)[timeIndex + 5: timeIndex + 7])
-        day = int(str(link.p)[timeIndex + 8: timeIndex + 10])
-        hour = int(str(link.p)[timeIndex + 11: timeIndex + 13])
-        minute = int(str(link.p)[timeIndex + 14: timeIndex + 16])
-        time = datetime.datetime(year, month, day, hour, minute)
-        timedifference = datetime.datetime.now() - time
-        #if the post timestamp is within the update rate, then it is considered a new post and displayed
-        if(timedifference.days == 0 and timedifference.seconds < UPDATE_RATE*60):
-            #finds the title of the post
-            titleStartIndex = str(link.p).find(".html") + 7
-            titleEndIndex = str(link.p).find("</a>", titleStartIndex, len(str(link.p)))
-            title = str(link.p)[titleStartIndex: titleEndIndex]
-            #finds the price
-            priceStartIndex = int(str(link.p).find("result-price") + 15)
-            priceEndIndex = str(link.p).find("</span>", priceStartIndex, len(str(link.p)))
-            #if there is no price, then default to 0
-            if (priceStartIndex > 100):
-                price = str(link.p)[priceStartIndex: priceEndIndex]
-            else:
-                price = 0
-            #finds the link
-            addressStartIndex = str(link.p).find("href") + 6
-            addressEndIndex = titleStartIndex - 2
-            address = str(link.p)[addressStartIndex: addressEndIndex]
-            
-            msg = msg + title + "\n" + "time: " + str(time) + "\nprice: " + price + "\n" + address + "\n\n\n\n"
+def sendMail(server, msg):
+    #if the message is empty, then dont send
+    if (len(msg) > 10):     
+        server.sendmail(username, recipient, msg)  
+        print("email sent")
+        server.quit()
+    else:
+        print("no new posts were found")
+        server.quit()
 
-            
-            
-print(msg)
-#if the message is empty, then dont send
-if (len(msg) > 10):     
-    server.sendmail(username, recipient, msg)  
-    print("email sent")
-    server.quit()
-else:
-    print("no new posts were found")
-    server.quit()
+if __name__ == "__main__":
+    connect(server, username, password)
+    parse()
+    sendMail(server, msg)
 
 
        
